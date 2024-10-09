7c2e0f2e5821f029ea5461e071c019c028b78a21
Gaston <gichuregaston@gmail.com>
2024-10-09 15:31:13 +0300

    Searching Elements In Binary Tree

---
+++ lib/binary_search_tree.dart
@@ -0,0 +1,55 @@
+import 'binary_node.dart';
+
+void main() {
+  final tree = buildExampleTree();
+  if (tree.contains(5)) {
+    print("Found 5!");
+  } else {
+    print("Couldn't find 5");
+  }
+}
+
+BinarySearchTree<int> buildExampleTree() {
+  var tree = BinarySearchTree<int>();
+  tree.insert(3);
+  tree.insert(1);
+  tree.insert(4);
+  tree.insert(0);
+  tree.insert(2);
+  tree.insert(5);
+  return tree;
+}
+
+class BinarySearchTree<E extends Comparable<dynamic>> {
+  BinaryNode<E>? root;
+
+  @override
+  String toString() => root.toString();
+
+  void insert(E value) {
+    root = _insertAt(root, value);
+  }
+
+  BinaryNode<E> _insertAt(BinaryNode<E>? node, E value) {
+    if (node == null) {
+      return BinaryNode(value);
+    }
+    if (value.compareTo(node.value) < 0) {
+      node.leftChild = _insertAt(node.leftChild, value);
+    } else {
+      node.rightChild = _insertAt(node.rightChild, value);
+    }
+    return node;
+  }
+
+  bool contains(E value) {
+    if (root == null) return false;
+    var found = false;
+    root!.traverseInOrder((other) {
+      if (value == other) {
+        found = true;
+      }
+    });
+    return found;
+  }
+}
