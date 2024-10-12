184eb92fc7c2c2adb93c5ddee116a82a4f65466a
Gaston <gichuregaston@gmail.com>
2024-10-12 09:24:00 +0300

    Function that checks if a binary tree is a binary search tree

---
+++ lib/binary_tree_challenge.dart
@@ -0,0 +1,16 @@
+import 'binary_node.dart';
+
+extension Check<E extends Comparable<dynamic>> on BinaryNode<E> {
+  bool isBinarySearchTree() {
+    return _isBST(this, min: null, max: null);
+  }
+
+  bool _isBST(BinaryNode<E>? tree, {E? min, E? max}) {
+    if (tree == null) return true;
+    if (min != null && tree.value.compareTo(min) < 0) {
+      return false;
+    }
+    return _isBST(tree.leftChild, min: min, max: tree.value) &&
+        _isBST(tree.rightChild, min: tree.value, max: max);
+  }
+}
