39c9c79e67722417f1d251429e7c09cf7d38e653
Gaston <gichuregaston@gmail.com>
2024-10-12 10:33:08 +0300

    Function to check equality of two binary trees

---
+++ lib/check_equality_of_binary_trees.dart
@@ -0,0 +1,15 @@
+import 'binary_node.dart';
+import 'binary_search_tree.dart';
+
+bool treesEqual(BinarySearchTree first, BinarySearchTree second) {
+  return _isEqual(first.root, second.root);
+}
+
+bool _isEqual(BinaryNode? first, BinaryNode? second) {
+  if (first == null || second == null) {
+    return first == null && second == null;
+  }
+  return first.value == second.value &&
+      _isEqual(first.leftChild, second.leftChild) &&
+      _isEqual(first.rightChild, second.rightChild);
+}
